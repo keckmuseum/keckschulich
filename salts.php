89d8968d8568871b7245bce6563800927d14cd2b
Josh Fleming <go@revoltmedia.com>
2017-07-23 01:16:23 -0700

    salt change

--- salts.php
+++ salts.php
@@ -8,4 +8,4 @@ define('NONCE_KEY',        'put your unique phrase here');
 define('AUTH_SALT',        'put your unique phrase here');
 define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here'); 

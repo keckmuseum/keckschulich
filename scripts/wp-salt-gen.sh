ff32b0e8c3ef68d1bee458fb6b35255f95fac736
Josh Fleming <go@revoltmedia.com>
2017-07-24 17:10:03 -0700

    salt generator script

--- scripts/wp-salt-gen.sh
+++ scripts/wp-salt-gen.sh
@@ -9,9 +9,9 @@ echo "writing salts to ${1}/${2}"
 find $1 -name $2 -print | while read line
 do
     curl http://api.wordpress.org/secret-key/1.1/salt/ > /tmp/wp_keys.txt
-    sed -i.bak -e '/put your unique phrase here/d' -e \
+    sed -i -e '/put your unique phrase here/d' -e \
     '/AUTH_KEY/d' -e '/SECURE_AUTH_KEY/d' -e '/LOGGED_IN_KEY/d' -e '/NONCE_KEY/d' -e \
     '/AUTH_SALT/d' -e '/SECURE_AUTH_SALT/d' -e '/LOGGED_IN_SALT/d' -e '/NONCE_SALT/d' $line
-    cat wp_keys.txt >> $line
+    cat /tmp/wp_keys.txt >> $line
     rm /tmp/wp_keys.txt
 done
